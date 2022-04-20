18545640ff86a7c2924130305aa13b8c12ebf876
yuan <2584278161@qq.com>
2022-04-21 00:54:02 +0800

    first commit

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="the-footer">
+        <p>毕 | 业 | 设 | 计</p>
+        <p>Yuan ©2022</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'the-footer'
+}
+</script>
+<style lang="scss" scoped>
+@import '../assets/css/the-footer.scss';
+</style>
